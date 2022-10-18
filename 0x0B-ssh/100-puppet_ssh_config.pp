8f0f9c633f7877c43ee1908feacb7884a3b97311
Al-hussary1 <Al-hussary@outlook.com>
2022-10-18 05:54:51 -0700

    0x0B-ssh

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,13 @@
-# connect to a server Execute a command
+ Setup a SSH client configuration file to connect to a server with RSA key.
 
-exec { 'echo "PasswordAuthentication no\nIdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config':
-        path    => '/bin/'
+file_line { 'Turn off passwd auth':
+  path    => '/etc/ssh/ssh_config',
+  line    => '    PasswordAuthentication no',
+  replace => true
+}
+
+file_line { 'Declare identity file':
+  path    => '/etc/ssh/ssh_config',
+  line    => '    IdentityFile ~/.ssh/school',
+  replace => true
 }
